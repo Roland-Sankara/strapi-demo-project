b404351b174800ef90513e3735bde25b9ade652e
gido311 <m.s.m.mokhtar@gmail.com>
2022-04-15 03:21:48 +0200

    intial commit

--- ghk-frontend/src/App.js
+++ ghk-frontend/src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <NavMenu />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Router>
+        <NavMenu />
+        <div>
+          <Routes>
+            <Route path="/" /*element={h}*/ />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
