c9b8deaed789e9b56809c838424a8c3919474086
gido311 <m.s.m.mokhtar@gmail.com>
2022-04-16 14:54:38 +0200

    second commit

--- ghk-frontend/src/App.js
+++ ghk-frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Router>
         <NavMenu />
-        <div>
+        <div className="App">
           <Routes>
             <Route path="/" /*element={<Home />}*/ />
           </Routes>
